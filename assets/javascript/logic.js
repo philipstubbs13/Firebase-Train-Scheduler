ccd2531f507a287b7dccb2867e344f2f9ad27a65
Phil S <philipstubbs13@gmail.com>
2018-01-06 20:03:15 -0600

    finishing initial UI design. Started writing javascript. Created click event for submit button.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -12,3 +12,22 @@ var config = {
 
  //Create database variable to create reference to firebase.database().
  var database = firebase.database();
+
+ //Click event for submit button. When user clicks Submit button to add a train to the schedule...
+ $("#submit-button").on("click", function() {
+
+	// Prevent form from submitting
+	event.preventDefault();
+
+	//Grab the values that the user enters in the text boxes in the "Add train" section. Store the values in variables.
+	var trainName = $("#train-name").val().trim();
+	var destination = $("#destination").val().trim();
+	var firstTrainTime = $("#first-train-time").val().trim();
+	var frequency = $("#frequency").val().trim();
+
+	//Print the values that the user enters in the text boxes to the console for debugging purposes.
+	console.log(trainName);
+	console.log(destination);
+	console.log(firstTrainTime);
+	console.log(frequency);
+});
