7eb0bf77b8f696aae3c891274f3a4c65c6d13ba0
Phil S <philipstubbs13@gmail.com>
2018-01-07 00:38:47 -0600

    added bootstrap tooltips

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -104,3 +104,8 @@ database.ref().on("child_added", function(snapshot) {
 		return;
 	}
 });
+
+//One way to initialize all tooltips on a page would be to select them by their data-toggle attribute:
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
